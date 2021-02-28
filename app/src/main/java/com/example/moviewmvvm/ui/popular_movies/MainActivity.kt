b8ff15e85606eb7e8479b1b86350de3b8b5f7e4f
BA RAVINDRA DANGWAL <BA.ravindradangwal@ndpl.com>
2021-02-28 11:08:59 +0530

    add space

--- app/src/main/java/com/example/moviewmvvm/ui/popular_movies/MainActivity.kt
+++ app/src/main/java/com/example/moviewmvvm/ui/popular_movies/MainActivity.kt
@@ -24,6 +24,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+         // add space for test git actions
    /*     btn.setOnClickListener{
 
             val intent= Intent(this, SingleMovie::class.java)
